b2a621044d6ed042a9e50dec78546c4a1b55f696
Ike Odira <odiraike8@gmail.com>
2022-09-21 18:01:07 +0100

    Create Search Exercise Component

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react'
+import {Box, Button, Stack, TextField, Typography} from '@mui/material'
+
+function SearchExercises() {
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+        <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'30px'}}} mb="50px" textAlign="center">Awesome Exercise <br/> You Should Know</Typography>
+        <Box position="relative" mb="72px">
+          <TextField
+            sx={{
+              input:{
+                fontWeight:'700', 
+                border:'none',
+                  borderRadius:'4px'
+            },
+            width:{lg:'800px', xs:'350px'},
+            backgroundColor:"#fff", 
+            borderRadius:'40px',
+          }}
+            height="76px"
+            value=""
+            onChange={(e)=>{console.log(e)}}
+            placeholder="Search Exercises"
+            type="text"
+          />
+          <Button variant="contained" className="search-btn" color="error" 
+              sx={{
+                // bgcolor:"#ff2625",
+                color:"#fff",
+                textTransform:"none",
+                width:{lg:'175px', xs:'80px'},
+                fontSize:{lg:'16px', xs:"14px"},
+                height:"56px", 
+                position:"absolute",
+                right:"0",
+              }}
+            >
+            Search
+          </Button>
+        </Box>
+    </Stack>
+  )
+}
+
+export default SearchExercises
